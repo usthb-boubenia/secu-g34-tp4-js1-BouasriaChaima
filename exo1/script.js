f041b9443008f69332ff214b1ed13d586c8e9220
BouasriaChaima <bouasriachaima362@gmail.com>
2025-11-04 18:16:53 +0100

    TP4 JavaScript termine

---
+++ exo1/script.js
@@ -0,0 +1,33 @@
+"use strict";//on utilise le use strict vu dans le slide 51
+//on utilise type const , vu on slide 37
+const Nom = "John";
+const Prenom = "Doe";
+const Age = 30;
+const Note = 15;
+const Pi = 3.14;
+// ici on utilise les fonctions vu dans le slide 45
+
+function f1() {
+    //ici on ytilise la sortie console.log vu dans le slide 33
+    console.log(Nom)
+}
+function f2() {
+    //ici on ytilise la sortie console.log vu dans le slide 33
+    console.log(Prenom)
+}
+function f3() {
+    //ici on ytilise la sortie console.log vu dans le slide 33
+    console.log(Nom + '-' + Prenom)
+}
+function f4() {
+    //ici on ytilise la sortie console.log vu dans le slide 33
+    console.log(Age)
+}
+function f5() {
+    //ici on ytilise la sortie console.log vu dans le slide 33
+    console.log(Note)
+}
+function f6() {
+    //ici on ytilise la sortie console.log vu dans le slide 33
+    console.log(Pi)
+}
---
+++ exo3/script.js
@@ -0,0 +1,54 @@
+"use strict";// vu dans le slide 51
+// ici on utilise un tableau d'objet(type complexe) , vu dans le slide 40
+const students = [
+    {
+        id: 1000,
+        nom: "JOHN",
+        prenom: "DOE",
+        note: 14,
+        absences: 5
+    },
+    {
+        id: 2000,
+        nom: "BOB",
+        prenom: "CARLTON",
+        note: 7,
+        absences: 1
+    },
+    {
+        id: 3000,
+        nom: "RAYANE",
+        prenom: "SMITH",
+        note: 13,
+        absences: 3
+    }
+];
+//ici on utilise la fonctions flechee, vu dans le slide 46
+const B = (moy) => {
+    // ici on utilise la condition, vu dans le slide 42
+    if (moy >= 10) {// et on utilise les operateurs, vu dans le slide 41
+        return true;
+    } else {
+        return false;
+    }
+};
+//ici on utilise les fonctions , vu dans le slide 45
+function A() {
+    let i = 0;//porte de la variable , vu dans le slide 51
+    //ici on utilise la boucle while, vu dans le slide 44
+    while (i < students.length) {
+        //porte de les variables , vu dans le slide 51
+        const estAdmis = B(students[i].note);
+        let statut;
+        // les conditions , vu dans le slide 42
+        if (estAdmis) {
+            statut = "ADMIS";
+        } else {
+            statut = "AJOURNE";
+        }
+        //ici on ytilise la sortie console.log vu dans le slide 33
+        console.log(students[i].id + ':' + statut);
+        // ici on utilise l'incrementation (operation) vu dans le slide 41
+        i++;
+    }
+};
